a17b010f7dc22647f905dc8ef353a4beef3aeb26
rs <chenrunshan@outlook.com>
2021-08-19 12:45:15 +0800

    fix

--- fetch.js
+++ fetch.js
@@ -5,8 +5,8 @@ const playwright = require('playwright')
 const main = async ()=> {
 	const browser = await playwright.chromium.launch({ args: ['--no-sandbox'] })
 	await Promise.all([
-		fetchDinoPools(browser),
-		fetchAdaPools(browser)
+    fetchAdaPools(browser).catch(e => console.log(e)),
+		fetchDinoPools(browser).catch(e => console.log(e))
 	])
 	await browser.close()
 }
